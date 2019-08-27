e4e97a51d90d7c6fc5a50fbefcb5a1d04c3d87bb
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-08-26 20:17:54 -0400

    Allow accessing project endpoint by df too, handle converting to id

--- R/get_mermaid_project_endpoint.R
+++ R/get_mermaid_project_endpoint.R
@@ -1,7 +1,7 @@
 #' Get endpoint from a specified MERMAID project.
 #'
 #' @inheritParams mermaid_GET
-#' @param project_id Project ID
+#' @param project A way to identify the project. Either a project ID (passed as a character vector directly), a single package resulting from \code{\link{get_mermaid_endpoint}} or \code{\link{search_projects}}
 #'
 #' @export
 #' @examples
@@ -12,7 +12,8 @@
 #' project_id <- dplyr::filter(projects, name == project_name)[["id"]]
 #' get_mermaid_project_endpoint(project_id, "sampleevents")
 #' }
-get_mermaid_project_endpoint <- function(project_id, endpoint = c("beltfishtransectmethods", "beltfishes", "benthiclittransectmethods", "benthicpittransectmethods", "benthicpits", "benthictransects", "collectrecords", "fishbelttransects", "habitatcomplexities", "obsbenthiclits", "obsbenthicpits", "obshabitatcomplexities", "obstransectbeltfishs", "managements", "observers", "profiles", "project_profiles", "sampleevents", "sites"), limit = 50, url = base_url, token = mermaid_token()) {
+get_mermaid_project_endpoint <- function(project, endpoint = c("beltfishtransectmethods", "beltfishes", "benthiclittransectmethods", "benthicpittransectmethods", "benthicpits", "benthictransects", "collectrecords", "fishbelttransects", "habitatcomplexities", "obsbenthiclits", "obsbenthicpits", "obshabitatcomplexities", "obstransectbeltfishs", "managements", "observers", "profiles", "project_profiles", "sampleevents", "sites"), limit = 50, url = base_url, token = mermaid_token()) {
+  project_id <- as_id(project)
   endpoint <- paste0("projects/", project_id, "/", endpoint)
   mermaid_GET(endpoint, limit = limit, url = url, token = mermaid_token())
 }
--- R/utils.R
+++ R/utils.R
@@ -17,3 +17,34 @@ check_limit <- function(limit) {
     limit
   }
 }
+
+as_id <- function(x) {
+  object_name <- deparse(substitute(x))
+
+  if (is.data.frame(x)) {
+    if (nrow(x) == 1) {
+      x <- check_id_in_df(x, object_name)
+      as.character(x[["id"]])
+    } else {
+      stop(paste0("`", object_name, "` must be a 1 row data frame or a length 1 character vector."),
+           call. = FALSE
+      )
+    }
+  } else if (!(is.vector(x) && length(x) == 1 && is.character(x))) {
+    stop(paste0("`", object_name, "` must be a 1 row data frame or a length 1 character vector."),
+         call. = FALSE
+    )
+  } else {
+    x
+  }
+}
+
+check_id_in_df <- function(x, name) {
+  if (!("id" %in% names(x))) {
+    stop(paste0("`", name, '` must contain a column "id".'),
+         call. = FALSE
+    )
+  } else {
+    x
+  }
+}
