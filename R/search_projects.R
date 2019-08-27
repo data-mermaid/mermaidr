a4567f358300faae4734c89f620e1559f7203e2e
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-08-26 20:28:22 -0400

    Add function for searching projects

---
+++ R/search_projects.R
@@ -0,0 +1,23 @@
+#' Search projects by name or ID
+#'
+#' Returns metadata on project, including ID, name, countries, number of sites, tags, notes, status, data sharing policies, and when the project was created and last updated.
+#'
+#' @param name Project name
+#' @param id Project ID
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun {
+#' search_projects(name = "mermaidr testing")
+#' }
+search_projects <- function(name = NULL, id = NULL) {
+  projects <- get_mermaid_endpoint("projects", limit = 99999)
+  if (!is.null(name)) {
+    projects <- projects[projects[["name"]] == name, ]
+  }
+  if (!is.null(id)) {
+    projects <- projects[projects[["id"]] == name, ]
+  }
+  projects
+}
