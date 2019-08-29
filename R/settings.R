8b9de2ed039b810b16edb9a72807602a0168f7f7
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-08-29 18:51:15 -0400

    Centralize API settings variables

---
+++ R/settings.R
@@ -0,0 +1,4 @@
+base_url <- "https://dev-api.datamermaid.org/"
+mermaid_authorize_url <- "https://datamermaid.auth0.com/authorize"
+mermaid_access_url <- "https://datamermaid.auth0.com/oauth/token"
+mermaid_key <- "6q1XvYG0n75ZaLbFko0gUV4xGud4uPyG"
--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,3 @@
-base_url <- "https://dev-api.datamermaid.org/"
-ua <- httr::user_agent("https://github.com/data-mermaid/mermaidr")
-
 check_limit <- function(limit) {
   if (length(limit) != 1) {
     stop("`limit` must be a length 1 positive integer vector.",
