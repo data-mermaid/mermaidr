97afd54b6d5ebe52bb5ea8e6bd8b0a1d5f5e6f0e
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-08-29 18:56:42 -0400

    Add removed user agent

--- R/settings.R
+++ R/settings.R
@@ -2,3 +2,4 @@ base_url <- "https://dev-api.datamermaid.org/"
 mermaid_authorize_url <- "https://datamermaid.auth0.com/authorize"
 mermaid_access_url <- "https://datamermaid.auth0.com/oauth/token"
 mermaid_key <- "6q1XvYG0n75ZaLbFko0gUV4xGud4uPyG"
+ua <- httr::user_agent("https://github.com/data-mermaid/mermaidr")
