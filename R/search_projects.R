28c370d5a95c14a314ce6042b5767e02b8fe81b2
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-09-17 14:34:32 -0400

    Change exact name argument

--- R/search_projects.R
+++ R/search_projects.R
@@ -4,15 +4,15 @@
 #'
 #' @param name Project name
 #' @param id Project ID
-#' @param fixed Whether the search should be by the exact \code{name} only (as opposed to the package name just needing to contain \code{name}). Defaults to FALSE.
+#' @param exact_name Whether the search should be by the exact \code{name} only (as opposed to the package name just needing to contain \code{name}). Defaults to FALSE, and is not used if \code{id} is supplied.
 #'
 #' @export
-search_projects <- function(name = NULL, id = NULL, fixed = FALSE) {
+search_projects <- function(name = NULL, id = NULL, exact_name = FALSE) {
   projects <- get_mermaid_endpoint("projects", limit = 99999)
   if (!is.null(id)) {
     projects[projects[["id"]] == id, ]
   } else if (!is.null(name)) {
-    if (fixed) {
+    if (exact_name) {
       projects[tolower(name) == tolower(projects[["name"]]), ]
     } else {
       projects[grepl(tolower(name), tolower(projects[["name"]])), ]
