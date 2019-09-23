9ec6bf9c4ee69bc59cbf5eff76b61e39e3e27605
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-09-23 13:10:00 -0400

    Full test coverage on search_projects

--- tests/testthat/test-search_projects.R
+++ tests/testthat/test-search_projects.R
@@ -3,6 +3,20 @@ test_that("search_projects throws a message if exact_name is TRUE and it returns
   expect_silent(search_projects("mermaid lagoon"))
 })
 
+test_that("search_projects returns a zero row tibble if the project is not found", {
+  name <- paste0(letters, collapse = "")
+  output <- search_projects(name = name)
+  expect_equal(nrow(output), 0)
+  expect_is(output, "tbl_df")
+
+  set.seed(1234)
+  id <- paste0(sample(1:1000, 3), collapse = "")
+  output <- search_projects(id = id)
+  expect_equal(nrow(output), 0)
+  expect_is(output, "tbl_df")
+}
+)
+
 test_that("check_single_project returns a warning if more than one result is returned", {
   x <- data.frame(a = 1:2)
   expect_message(check_single_project(x, "test"))
