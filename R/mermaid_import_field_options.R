81ed306ffdaf552dd220e333576971a6fe56b7b7
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-06-18 14:43:51 -0400

    Enable checking options against values

--- R/mermaid_import_field_options.R
+++ R/mermaid_import_field_options.R
@@ -3,16 +3,17 @@
 #' Check the options available for a given field in MERMAID, either to see what the options are or to check that your data matches the available fields.
 #'
 #' @param field Field to check options for. One of: width, fishsizebin, reefslope, visibility, current, relativedepth, tide, fishname, benthicattribute, growthform, habitatcomplexityscore.
-#' @param matches Optional options from your data, to match against valid options in Collect
+#' @param values Optional values from your data, to match against valid options in Collect
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' mermaid_import_field_options("growthform")
+#'
+#' mermaid_import_field_options("growthform", c("Arboresent", "Branching", "Columar", "Foliose"))
 #' }
-mermaid_import_field_options <- function(field = c("width", "fishsizebin", "reefslope", "visibility", "current", "relativedepth", "tide", "fishname", "benthicattribute", "growthform", "habitatcomplexityscore"), matches) {
-
+mermaid_import_field_options <- function(field = c("width", "fishsizebin", "reefslope", "visibility", "current", "relativedepth", "tide", "fishname", "benthicattribute", "growthform", "habitatcomplexityscore"), values) {
   field_options <- c("width", "fishsizebin", "reefslope", "visibility", "current", "relativedepth", "tide", "fishname", "benthicattribute", "growthform", "habitatcomplexityscore")
 
   if (!field %in% field_options) {
@@ -33,17 +34,32 @@ mermaid_import_field_options <- function(field = c("width", "fishsizebin", "reef
       dplyr::filter(.data$name == internal_field_name) %>%
       dplyr::pull(.data$data) %>%
       purrr::pluck(1) %>%
-      dplyr::select(value = .data$name)
-
+      dplyr::select(mermaid_value = .data$name)
   } else if (internal_field_name == "fishspecies") {
     res <- get_endpoint(internal_field_name) %>%
       dplyr::select(value = .data$display)
   } else if (internal_field_name == "benthicattributes") {
     res <- get_endpoint(internal_field_name) %>%
-      dplyr::select(value = .data$name)
+      dplyr::select(mermaid_value = .data$name)
   }
 
-  if (missing(matches)) {
+  # If not checking any options, just return the possible values
+  if (missing(values)) {
     return(res)
   }
+
+  # Otherwise, check them!
+  values_df <- dplyr::tibble(value = unique(values))
+
+  values_df %>%
+    fuzzyjoin::stringdist_left_join(res,
+      by = c("value" = "mermaid_value"),
+      distance_col = "diff", max_dist = Inf
+    ) %>%
+    dplyr::group_by(.data$value) %>%
+    dplyr::filter(diff == min(.data$diff)) %>%
+    dplyr::filter(dplyr::row_number() == 1) %>%
+    dplyr::mutate(match = .data$diff == 0) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(.data$value, closest_mermaid_value = .data$mermaid_value, .data$match)
 }
