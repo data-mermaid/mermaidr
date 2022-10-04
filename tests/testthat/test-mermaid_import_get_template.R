b008d0522e3ab093a4b011bbd384201259f969c5
Sharla Gelfand <sharla.gelfand@gmail.com>
2022-10-04 09:35:21 -0400

    R cmd check fixes

--- tests/testthat/test-mermaid_import_get_template.R
+++ tests/testthat/test-mermaid_import_get_template.R
@@ -1,4 +1,8 @@
 test_that("mermaid_import_get_template returns a single, 0 row named tibble if there is one (not 'all') method passed", {
+  skip_if_offline()
+  skip_on_ci()
+  skip_on_cran()
+
   template <- mermaid_import_get_template("fishbelt")
   expect_s3_class(template, "tbl_df")
   expect_true(nrow(template) == 0)
@@ -7,6 +11,10 @@ test_that("mermaid_import_get_template returns a single, 0 row named tibble if t
 
 test_that("mermaid_import_get_template returns a list of 0 row named tibbles if multiple methods are passed", {
   methods <- c("fishbelt", "benthicpit")
+  skip_if_offline()
+  skip_on_ci()
+  skip_on_cran()
+
   template <- mermaid_import_get_template(methods)
   expect_named(template, methods)
   purrr::walk(template, ~ expect_true(nrow(.x) == 0))
@@ -14,6 +22,10 @@ test_that("mermaid_import_get_template returns a list of 0 row named tibbles if 
 })
 
 test_that("mermaid_import_get_template returns a list of 0 row named tibbles, one for every method, if 'all' is passed", {
+  skip_if_offline()
+  skip_on_ci()
+  skip_on_cran()
+
   template <- mermaid_import_get_template("all")
   expect_named(template, c("fishbelt", "benthiclit", "benthicpit", "benthicpqt", "bleachingqc", "habitatcomplexity"))
   purrr::walk(template, ~ expect_true(nrow(.x) == 0))
