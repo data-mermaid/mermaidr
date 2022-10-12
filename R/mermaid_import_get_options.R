ad205310b1dcca262a7800506e078b5332d9025d
Sharla Gelfand <sharla.gelfand@gmail.com>
2022-10-12 09:13:10 -0400

    Docs

--- R/mermaid_import_get_options.R
+++ R/mermaid_import_get_options.R
@@ -1,5 +1,48 @@
-mermaid_import_get_options <- function(project, method = c("fishbelt", "benthiclit", "benthicpit", "benthicpqt", "bleachingqc", "habitatcomplexity", "all"), token = mermaid_token()) {
+#' Get field options for MERMAID import
+#'
+#' Check the options available for importing a given method into MERMAID, either to see what the options are for each field or to check that your data matches the available fields. Returns a list of each field for a given method, whether it's required (in \code{required}), and a list of available choices, if relevant (in \code{choices}).  Optionally, the fields and options can be saved into an Excel file using the \code{save} parameter.
+#'
+#' @param method Method to get import fields and options for. One of "fishbelt", "benthiclit", "benthicpit", "benthicpqt", "bleaching", "habitatcomplexity", or "all" (to get fields for all methods).
+#' @param save
+#' @inheritParams get_project_endpoint
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' options <- mermaid_get_my_projects() %>%
+#'   head(1) %>%
+#'   mermaid_import_get_options("fishbelt")
+#'
+#' names(options)
+#' # [1] "Site *"                     "Management *"
+#' # [3] "Sample date: Year *"        "Sample date: Month *"
+#' # [5] "Sample date: Day *"         "Sample time"
+#' # [7] "Depth *"                    "Transect number *"
+#' # [9] "Transect label"             "Transect length surveyed *"
+#' # [11] "Width *"                    "Fish size bin *"
+#' # [13] "Reef slope"                 "Visibility"
+#' # [15] "Current"                    "Relative depth"
+#' # [17] "Tide"                       "Sample unit notes"
+#' # [19] "Observer emails *"          "Fish name *"
+#' # [21] "Size *"                     "Count *"
+#'
+#' options[["Reef slope"]][["required"]]
+#' # [1] FALSE
+#'
+#' options[["Reef slope"]][["choices"]]
+#' # A tibble: 4 × 1
+#' # value
+#' # <chr>
+#' # 1 crest
+#' # 2 flat
+#' # 3 slope
+#' # 4 wall
+#' }
+mermaid_import_get_options <- function(project, method = c("fishbelt", "benthiclit", "benthicpit", "benthicpqt", "bleaching", "habitatcomplexity", "all"), save = NULL, token = mermaid_token()) {
   check_project_data_inputs(method, data = "all") # Faking data input to just allow for checking options
+
+  method[method == "bleaching"] <- "bleachingqc"
 
   if (any(method == "all")) {
     method <- c("fishbelt", "benthiclit", "benthicpit", "benthicpqt", "bleachingqc", "habitatcomplexity")
--- R/mermaid_import_get_template.R
+++ R/mermaid_import_get_template.R
@@ -1,4 +1,15 @@
-mermaid_import_get_template <- function(method = c("fishbelt", "benthiclit", "benthicpit", "benthicpqt", "bleachingqc", "habitatcomplexity", "all")) {
+#' Get template for MERMAID import
+#'
+#' Get a template of the fields for importing into MERMAID. Used along with \code{\link{mermaid_import_get_options}}, which contains details on which fields are required and their possible options, if relevant. Optionally, the template can be saved into an Excel file using the \code{save} parameter.
+#'
+#' @param method Method to get import fields for. One of "fishbelt", "benthiclit", "benthicpit", "benthicpqt", "bleaching", "habitatcomplexity", or "all" (to get fields for all methods).
+#' @param save
+#'
+#' @return
+#' @export
+#'
+#' @examples
+mermaid_import_get_template <- function(method = c("fishbelt", "benthiclit", "benthicpit", "benthicpqt", "bleachingqc", "habitatcomplexity", "all"), save = NULL) {
   check_project_data_inputs(method, data = "all") # Faking data input to just allow for checking options
 
   if (any(method == "all")) {
