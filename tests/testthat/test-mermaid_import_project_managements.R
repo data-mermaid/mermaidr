bf03d2705ba72609fc61cc86a687be8b82b42967
Sharla Gelfand <sharla.gelfand@gmail.com>
2023-01-27 14:36:29 -0500

    Leave placeholder, failing tests, for more tests to add

--- tests/testthat/test-mermaid_import_project_managements.R
+++ tests/testthat/test-mermaid_import_project_managements.R
@@ -367,3 +367,11 @@ test_that("no take AND partial restrictions, a conflict, errors", {
 
   expect_error(mermaid_import_project_managements("2c0c9857-b11c-4b82-b7ef-e9b383d1233c", data), "Cannot have both `no_take` and any partial restrictions rules")
 })
+
+test_that("Multiple managements import properly", {
+  expect_true(FALSE)
+})
+
+test_that("Multiple managements, with some failures, show row of error", {
+  expect_true(FALSE)
+})
