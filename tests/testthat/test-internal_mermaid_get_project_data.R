b90c55410ceafed822d658efdec9fe82de5cfdea
Sharla Gelfand <sharla.gelfand@gmail.com>
2023-02-22 13:32:43 -0500

    Comparisong JSON vs CSV

--- tests/testthat/test-internal_mermaid_get_project_data.R
+++ tests/testthat/test-internal_mermaid_get_project_data.R
@@ -1,24 +1,44 @@
+# - No permission to export data on dev APP for 2018_Vatu-i-Ra reef surveys
+# - Discrepancy in how "community/local government"  is represented in CSV versus API/JSON - it's "community, local government" in CSV (https://dev-api.datamermaid.org/v1/projects/170e7182-700a-4814-8f1e-45ee1caf3b44/beltfishes/obstransectbeltfishes/csv/ vs https://dev-api.datamermaid.org/v1/projects/170e7182-700a-4814-8f1e-45ee1caf3b44/beltfishes/obstransectbeltfishes/)
+# - [] for empty management_rules instead of NA, but it should not even be possible - total edge case
+# - API/JSON sample units has columns that CSV does not - biomass_kgha_fish_family_zeroes_* and. a bunch of other  biomass_kgha_fish_family_* that are not in CSV
+# - One OLD fishbelt sampleunits has tide NA, NEW has tide rising - i think some observations have rising, so it is concatenated in new? project == "Great Sea Reef 2019", site == "CH2B". same with current for same project/site
+# - project XPDC Kei Kecil 2018, site KE10 has mix of visibility - 1-5m - poor and 5-10m - fair, JSON API gives 1-5m poor, CSV gives 5-10m - fair - same with depth, transect_width for same project/site
+# - for same project/site, biomass from JSON is 272 but 29.7 in CSV (and this matches in app)
+
 test_that("new method of using CSV endpoint produces same data as old method (using JSON)", {
-  p <- "02e6915c-1c64-4d2c-bac0-326b560415a2"
-  new <- mermaid_get_project_data(p, method = "fishbelt", data = "observations")
-  old <- mermaid_get_project_data_legacy(p, method = "fishbelt", data = "observations")
+  p <- mermaid_get_my_projects()
+  old <- mermaid_get_project_data_legacy(p, method = "fishbelt", data = "all", limit = 50)
+  new <- mermaid_get_project_data(p, method = "fishbelt", data = "all", limit = 50)
 
-  # # Some conversion required - old has empty strings ("") while new has NA, difference in column types
-  # old <- old %>% dplyr::mutate_all(as.character)
-  # old <- old %>% dplyr::mutate_all(~ ifelse(.x == "", NA_character_, .x))
-  # old <- old %>% dplyr::mutate_all(as.character)
-  # new <- new %>% dplyr::mutate_all(as.character)
+  expect_identical(
+    old[["observations"]] %>%
+      dplyr::arrange(project, site, management_parties, sample_date, sample_time, relative_depth, transect_number, fish_family, fish_genus, fish_taxon, count) %>%
+      dplyr::mutate_if(is.numeric, round, 3),
+    new[["observations"]] %>%
+      dplyr::arrange(project, site, management_parties, sample_date, sample_time, relative_depth, transect_number, fish_family, fish_genus, fish_taxon, count) %>%
+      dplyr::mutate_if(is.numeric, round, 3)
+  )
 
-  expect_identical(old, new)
+  old_su <- old[["sampleunits"]] %>%
+    dplyr::select(tidyselect::all_of(names(new[["sampleunits"]]))) %>%
+    dplyr::arrange(project, management, site, sample_date, sample_time, tide, current, visibility, relative_depth, depth, reef_slope, transect_number) %>%
+    dplyr::mutate_if(is.numeric, round, 2) %>%
+    filter(!(project == "Great Sea Reef 2019" & site == "CH2B")) %>%
+    filter(!(project == "XPDC Kei Kecil 2018" & site == "KE10"))
+  new_su <- new[["sampleunits"]] %>%
+    dplyr::arrange(project, management, site, sample_date, sample_time, tide, current, visibility, relative_depth, depth, reef_slope, transect_number) %>%
+    dplyr::mutate_if(is.numeric, round, 2) %>%
+    filter(!(project == "Great Sea Reef 2019" & site == "CH2B")) %>%
+    filter(!(project == "XPDC Kei Kecil 2018" & site == "KE10"))
 
-  new <- mermaid_get_project_data(p, method = "fishbelt", data = "sampleunits")
-  old <- mermaid_get_project_data_legacy(p, method = "fishbelt", data = "sampleunits")
+  expect_identical(old_su, new_su)
 
-  # Some conversion required - old has empty strings ("") while new has NA, difference in column types
-  old <- old %>% dplyr::mutate_all(as.character)
-  old <- old %>% dplyr::mutate_all(~ ifelse(.x == "", NA_character_, .x))
-  old <- old %>% dplyr::mutate_all(as.character)
-  new <- new %>% dplyr::mutate_all(as.character)
-
-  expect_identical(old, new)
+  expect_identical(
+    old[["sampleevents"]] %>%
+      dplyr::select(tidyselect::all_of(names(new[["sampleevents"]]))) %>%
+      dplyr::mutate_if(is.numeric, round, 2),
+    new[["sampleevents"]] %>%
+      dplyr::mutate_if(is.numeric, round, 2)
+  )
 })
