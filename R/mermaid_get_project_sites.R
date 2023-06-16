69598eb7b7dbc3b3961c84f863e1a3d0fd5751fe
Sharla Gelfand <sharla.gelfand@gmail.com>
2023-06-16 11:23:59 -0400

    Add option to get covariates in project_sites()

--- R/mermaid_get_project_sites.R
+++ R/mermaid_get_project_sites.R
@@ -4,6 +4,7 @@
 #'
 #' @inheritParams get_project_endpoint
 #' @inheritParams mermaid_GET
+#' @covariates Whether to include covariates (Allen Coral Atlas, Andrello, Beyer). Defaults to FALSE.
 #'
 #' @export
 #'
@@ -17,4 +18,10 @@ mermaid_get_project_sites <- function(project = mermaid_get_default_project(), l
   get_project_endpoint(project = project, endpoint = "sites", limit = limit, token = token, filter = list(covars = covariates))
 }
 
-project_sites_columns <- c("id", "name", "notes", "latitude", "longitude", "country", "reef_type", "reef_zone", "exposure", "created_on", "updated_on")
+project_sites_columns <- c(
+  "id", "name", "notes", "latitude", "longitude", "country", "reef_type", "reef_zone", "exposure",
+  # Covariates
+  "aca_geomorphic", "aca_benthic", "andrello_grav_nc", "andrello_sediment", "andrello_nutrient", "andrello_pop_count", "andrello_num_ports", "andrello_reef_value", "andrello_cumul_score", "beyer_score", "beyer_scorecn", "beyer_scorecy", "beyer_scorepfc", "beyer_scoreth", "beyer_scoretr",
+
+  "created_on", "updated_on"
+)
