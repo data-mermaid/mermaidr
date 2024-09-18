667355c55b57aeba6e52e27b5868b511578d8ccb
Sharla Gelfand <sharla.gelfand@gmail.com>
2024-09-18 10:35:30 -0400

    Handle non-valid providers

--- R/mermaid_get_classification_labelmappings.R
+++ R/mermaid_get_classification_labelmappings.R
@@ -1,9 +1,9 @@
 #' Classification label mappings
 #'
-#' Get a lookup table that maps MERMAID Benthic Attributes and Growth Forms to labels from other providers (e.g. CoralNet, ReefCloud).
+#' Get a lookup table that maps MERMAID Benthic Attributes and Growth Forms to labels from other providers (CoralNet and ReefCloud).
 #'
 #' @inheritParams mermaid_GET
-#' @param provider Provider to filter the lookup table for, e.g. CoralNet or ReefCloud.
+#' @param provider Provider to filter the lookup table for. Allowed options are CoralNet and ReefCloud.
 #'
 #' @export
 #'
@@ -11,7 +11,12 @@
 #' mermaid_get_classification_labelmappings()
 mermaid_get_classification_labelmappings <- function(provider = NULL, limit = NULL) {
   endpoint <- "classification/labelmappings"
+
   if (!is.null(provider)) {
+    # Error if the provider is not CoralNet or ReefCloud
+    if (length(provider) != 1 | !all(provider %in% allowed_providers)) {
+      stop(glue::glue('`provider` must be one of: "{providers}"', providers = glue::glue_collapse(allowed_providers, sep = '", "')), call. = FALSE)
+    }
     get_endpoint(endpoint = endpoint, limit = limit, filter = list(provider = provider))
   } else {
     get_endpoint(endpoint = endpoint, limit = limit)
@@ -19,3 +24,5 @@ mermaid_get_classification_labelmappings <- function(provider = NULL, limit = NU
 }
 
 classification_labelmappings_columns <- c("id", "benthic_attribute", "growth_form", "provider_id", "provider_label", "provider")
+
+allowed_providers <- c("CoralNet", "ReefCloud")
