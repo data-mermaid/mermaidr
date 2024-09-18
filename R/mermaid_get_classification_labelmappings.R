234ff4e5570d60725ef578d148c763a92eb022c0
Sharla Gelfand <sharla.gelfand@gmail.com>
2024-09-18 10:20:06 -0400

    Add provider filter

--- R/mermaid_get_classification_labelmappings.R
+++ R/mermaid_get_classification_labelmappings.R
@@ -1,15 +1,21 @@
 #' Classification label mappings
 #'
-#' Get a lookup table that maps MERMAID Benthic Attributes and Growth Forms to labels from other sources (e.g. CoralNet).
+#' Get a lookup table that maps MERMAID Benthic Attributes and Growth Forms to labels from other providers (e.g. CoralNet, ReefCloud).
 #'
 #' @inheritParams mermaid_GET
+#' @param provider Provider to filter the lookup table for, e.g. CoralNet or ReefCloud.
 #'
 #' @export
 #'
 #' @examples
 #' mermaid_get_classification_labelmappings()
-mermaid_get_classification_labelmappings <- function(limit = NULL) {
-  get_endpoint(endpoint = "classification/labelmappings", limit = limit)
+mermaid_get_classification_labelmappings <- function(provider = NULL, limit = NULL) {
+  endpoint <- "classification/labelmappings"
+  if (!is.null(provider)) {
+    get_endpoint(endpoint = endpoint, limit = limit, filter = list(provider = provider))
+  } else {
+    get_endpoint(endpoint = endpoint, limit = limit)
+  }
 }
 
 classification_labelmappings_columns <- c("id", "benthic_attribute", "growth_form", "provider_id", "provider_label", "provider")
