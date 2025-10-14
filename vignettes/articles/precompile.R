45b899a2c331fb3c212fa5ba482c709fedac2ca2
Sharla Gelfand <sharla.gelfand@gmail.com>
2025-10-14 17:24:11 -0400

    Move data to extdata

--- vignettes/articles/precompile.R
+++ vignettes/articles/precompile.R
@@ -1,7 +1,8 @@
 # Precompiled vignettes that depend on API key
 # Must manually move image files to vignettes/articles/ after knit
 
-knitr::knit("vignettes/articles/import_cpce.Rmd.orig", output = "vignettes/articles/import_cpce.Rmd")
 knitr::knit("vignettes/articles/accessing_project_data.Rmd.orig", output = "vignettes/articles/accessing_project_data.Rmd")
-# This one is on prod, not dev
+
+# On prod, not dev
 knitr::knit("vignettes/articles/importing_fishbelt.Rmd.orig", output = "vignettes/articles/importing_fishbelt.Rmd")
+knitr::knit("vignettes/articles/import_cpce.Rmd.orig", output = "vignettes/articles/import_cpce.Rmd")
