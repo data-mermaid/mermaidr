f992f03c6037dfbbca231779ee6158cf96c8e0c3
Sharla Gelfand <sharla.gelfand@gmail.com>
2025-12-17 13:45:53 -0500

    Render vignette on prod

--- vignettes/articles/precompile.R
+++ vignettes/articles/precompile.R
@@ -2,8 +2,8 @@
 # Must manually move image files to vignettes/articles/ after knit
 
 knitr::knit("vignettes/articles/accessing_project_data.Rmd.orig", output = "vignettes/articles/accessing_project_data.Rmd")
-knitr::knit("vignettes/articles/import_coralnet.Rmd.orig", output = "vignettes/articles/import_coralnet.Rmd")
 
 # On prod, not dev
 knitr::knit("vignettes/articles/importing_fishbelt.Rmd.orig", output = "vignettes/articles/importing_fishbelt.Rmd")
 knitr::knit("vignettes/articles/import_cpce.Rmd.orig", output = "vignettes/articles/import_cpce.Rmd")
+knitr::knit("vignettes/articles/import_coralnet.Rmd.orig", output = "vignettes/articles/import_coralnet.Rmd")
