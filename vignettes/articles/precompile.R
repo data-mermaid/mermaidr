e4fc93518c357f90faaa150112464584afeef9c1
Sharla Gelfand <sharla.gelfand@gmail.com>
2025-12-17 13:26:31 -0500

    Precompile rmd

--- vignettes/articles/precompile.R
+++ vignettes/articles/precompile.R
@@ -2,6 +2,7 @@
 # Must manually move image files to vignettes/articles/ after knit
 
 knitr::knit("vignettes/articles/accessing_project_data.Rmd.orig", output = "vignettes/articles/accessing_project_data.Rmd")
+knitr::knit("vignettes/articles/import_coralnet.Rmd.orig", output = "vignettes/articles/import_coralnet.Rmd")
 
 # On prod, not dev
 knitr::knit("vignettes/articles/importing_fishbelt.Rmd.orig", output = "vignettes/articles/importing_fishbelt.Rmd")
