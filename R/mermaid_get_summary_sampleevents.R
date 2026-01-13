a28d6d8c04d08d7e8fbf6a6cf28ad42077b35610
Sharla Gelfand <sharla.gelfand@gmail.com>
2026-01-13 12:17:00 -0500

    Add columns to get_projects

--- R/mermaid_get_summary_sampleevents.R
+++ R/mermaid_get_summary_sampleevents.R
@@ -1,6 +1,10 @@
 #' Get aggregated metrics for all MERMAID surveys, by site, by date
 #'
-#' Get aggregated metrics from all surveys associated with each site, by date. Includes data from all methods - Fish Belt, Benthic LIT, Benthic PIT, Benthic Photo Quadrat, Bleaching, and Habitat Complexity, - if the data sharing policy for that method is public summary or public (and just includes the sample unit count otherwise). Does not require authorization.
+#' Get aggregated metrics from all surveys associated with each site, by date.
+#' Includes data from all methods -- Fish Belt, Benthic LIT, Benthic PIT,
+#' Benthic Photo Quadrat, Bleaching, and Habitat Complexity -- if the data sharing
+#' policy for that method is public summary or public (and just includes the sample
+#'  unit count otherwise). Does not require authorization.
 #'
 #' @inheritParams mermaid_GET
 #'
