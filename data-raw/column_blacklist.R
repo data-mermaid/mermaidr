894d9ec90699ccc601a3957a10a024805b268b09
Sharla Gelfand <sharla.gelfand@gmail.com>
2026-08-03 12:30:33 -0400

    inverts refs

--- data-raw/column_blacklist.R
+++ data-raw/column_blacklist.R
@@ -7,3 +7,21 @@ usethis::use_data(blacklist_columns, overwrite = TRUE)
 legacy_columns <- read_yaml(here::here("data-raw", "legacy_columns.yml"))
 
 usethis::use_data(legacy_columns, overwrite = TRUE)
+
+tested_endpoints <- c(
+  "projects",
+  "sites",
+  "managements",
+  "choices",
+  "benthicattributes",
+  "fishfamilies",
+  "fishgenera",
+  "fishspecies",
+  "fishsizes",
+  "projecttags",
+  "me",
+  "invertattributes",
+  "invertspecies"
+)
+
+usethis::use_data(tested_endpoints, overwrite = TRUE)
