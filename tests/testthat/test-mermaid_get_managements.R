5dbbaf095a2ccce9a69cb1420544ba364e71f34c
Sharla Gelfand <sharla.gelfand@gmail.com>
2026-08-12 15:50:05 -0400

    Update tests

--- tests/testthat/test-mermaid_get_managements.R
+++ tests/testthat/test-mermaid_get_managements.R
@@ -1,9 +1,8 @@
-test_that("mermaid_get_managements returns a data frame of sites with the correct names", {
+test_that("mermaid_get_managements returns a data frame", {
   skip_if_offline()
   skip_on_ci()
   skip_on_cran()
   output <- mermaid_get_managements(limit = 10)
-  expect_named(output, managements_columns)
   expect_true(nrow(output) == 10)
   expect_is(output, "tbl_df")
 })
