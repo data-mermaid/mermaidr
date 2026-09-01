fe6830261ddcbc19992b8d61314e69a11f2e67f6
Sharla Gelfand <sharla.gelfand@gmail.com>
2026-09-01 11:11:47 -0400

    Make legacy/blacklist datasets internal only

--- data-raw/column_blacklist.R
+++ data-raw/column_blacklist.R
@@ -2,8 +2,8 @@ library(yaml)
 
 blacklist_columns <- read_yaml(here::here("data-raw", "column_blacklist.yml"))
 
-usethis::use_data(blacklist_columns, overwrite = TRUE)
+usethis::use_data(blacklist_columns, overwrite = TRUE, internal = TRUE)
 
 legacy_columns <- yaml::read_yaml(here::here("data-raw/legacy_columns.yml"))
 
-usethis::use_data(legacy_columns, overwrite = TRUE)
+usethis::use_data(legacy_columns, overwrite = TRUE, internal = TRUE)
